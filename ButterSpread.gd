46d2c7de51c643b8426e5e12c291df9fcd5d51e7
Piotr Klimaszewski <pixelarious@gmail.com>
2022-07-30 19:57:54 +0200

    yummy, butterSpread added

---
+++ ButterSpread.gd
@@ -0,0 +1,26 @@
+extends CPUParticles2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Timer_timeout():
+	set_process(false)
+	set_process_internal(false)
+	set_physics_process(false)
+	set_process_input(false)
+	set_process_unhandled_input(false)
+	set_process_unhandled_key_input(false)
+	self.speed_scale = 0
