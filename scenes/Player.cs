a023d6eaa178a97b52220904292f1ee345ca4cbc
Jakub Latusek <jk.latusek@gmail.com>
2022-07-30 19:52:34 +0200

    Add boost move

--- scenes/Player.cs
+++ scenes/Player.cs
@@ -4,42 +4,48 @@ using System;
 public class Player : GravityObject
 {
 	public Player() : base(JUMP_FORCE) { }
-	const int MAX_SPEED = 100;
-	const int ACCEL = 30;
+	const int MAX_SPEED = 150;
+	const int MAX_SPEED_BOOST = (int)(MAX_SPEED * 1.5f);
+	const int ACCELERATION_DEFAULT = 30;
 	const int JUMP_FORCE = 700;
 	enum Direction { right, left, down, up }
 
 	private Vector2 motion = new Vector2();
 	private Vector2_t<Direction> direction = new Vector2_t<Direction>(Direction.right, Direction.down);
 	private JumpPhase jump_phase = JumpPhase.Idle;
-
+	private int max_speed = MAX_SPEED;
 	override public void _Ready()
 	{
 		GD.Print("Hello from C# to Godot :)");
 	}
 	override public void _Process(float delta)
 	{
-		if (Input.IsActionPressed("ui_right"))
+
+		if (Input.IsActionJustPressed("mv_accelerate"))
 		{
-			direction.x = Direction.right;
+			max_speed = MAX_SPEED_BOOST;
+		}
+		else if (Input.IsActionJustReleased("mv_accelerate"))
+		{
+			max_speed = MAX_SPEED;
 		}
 
 		if (Input.IsActionPressed("mv_left"))
 		{
-			motion.x -= ACCEL;
+			motion.x -= ACCELERATION_DEFAULT;
 			direction.x = Direction.left;
 		}
 		else if (Input.IsActionPressed("mv_right"))
 		{
-			motion.x += ACCEL;
+			motion.x += ACCELERATION_DEFAULT;
 			direction.x = Direction.right;
 		}
 		else
 		{
-			motion.x = Mathf.Lerp(motion.x, 0, 0.2f);
+			motion.x = Mathf.Lerp(motion.x, 0, 0.3f);
 		}
 
-		motion.x = Mathf.Clamp(motion.x, -MAX_SPEED, MAX_SPEED);
+		motion.x = Mathf.Clamp(motion.x, -max_speed, max_speed);
 
 		if (Input.IsActionPressed("mv_up") && IsOnFloor())
 		{
