27220d4c6bc2ef28598b729f56f57bb2a4c8b566
Piotr Klimaszewski <pixelarious@gmail.com>
2022-07-31 21:22:11 +0200

    Basic title screen and menu.

---
+++ scenes/menus/InfoScreen.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$BackButton.grab_focus()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_BackButton_pressed():
+	get_tree().change_scene("res://scenes/menus/TitleScreen.tscn")
---
+++ scenes/menus/TitleScreen.gd
@@ -0,0 +1,25 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$VSplitContainer/AnimationPlayer.play("TitleAnim")
+	$VSplitContainer/AnimationPlayer2/Menu.set_position(Vector2(0, 90))
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "TitleAnim":
+		$VSplitContainer/AnimationPlayer.play("TitleUp")
+		$VSplitContainer/AnimationPlayer2.play("MenuUp")
+	#elif anim_name == "TitleUp":
+	#	get_tree().change_scene("res://scenes/menus/Menu.tscn")
