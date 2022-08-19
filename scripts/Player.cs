94fbd3a47183d1fdfc439cd6e45f9b17815796f0
Jakub Latusek <jk.latusek@gmail.com>
2022-08-19 23:15:58 +0200

    Add simple dash

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -2,8 +2,7 @@ using Godot;
 
 public class Player : GravityObject
 {
-	public Player() : base(JUMP_FORCE) { 
-		dash_timer.WaitTime = 1.0f;
+	public Player() : base(JUMP_FORCE) {
 	}
 	const int MAX_SPEED = 150;
 	const int MAX_SPEED_BOOST = (int)(MAX_SPEED * 1.7f);
@@ -16,7 +15,7 @@ public class Player : GravityObject
 	private JumpPhase jump_phase = JumpPhase.Idle;
 	private int max_speed = MAX_SPEED;
 
-	private Timer dash_timer = new Timer();
+	private Timer dash_timer;
 
 	public bool IsMoving()
 	{
@@ -42,10 +41,36 @@ public class Player : GravityObject
 
 	public void Dash()
 	{
-		if (dash_timer.IsStopped())
+		if (dash_timer is null)
 		{
+			dash_timer = GetNodeOrNull<Timer>("dash_timer");
+		}
+		if (dash_timer != null && dash_timer.IsStopped())
+		{
+
 			dash_timer.Start();
 			motion.x = direction.x == Direction.right ? MAX_SPEED * 2 : -MAX_SPEED * 2;
+
+			var butter_dash = ResourceLoader.Load<PackedScene>("res://scenes/DashEffect.tscn");
+			var butter_dash_instance = butter_dash.InstanceOrNull<DashEffect>();
+			if (butter_dash_instance != null)
+			{
+				GetTree().CurrentScene.AddChild(butter_dash_instance);
+				butter_dash_instance.GlobalPosition = new Vector2(GlobalPosition.x, GlobalPosition.y);
+				butter_dash_instance.ZIndex = 3;
+				if(Direction.right == direction.x)
+				{
+					butter_dash_instance.Scale = new Vector2(-1, 1);
+				}
+				if(Direction.left == direction.x)
+				{
+					butter_dash_instance.Scale = new Vector2(1, 1);
+				}
+
+				butter_dash_instance.Emitting = true;
+
+				GD.Print("Dash effect called");
+			}
 		}
 	}
 
