3cdfd744441cff1f1124747708452f79c45041c5
piokli <49364948+piokli@users.noreply.github.com>
2022-09-02 12:45:03 +0200

    Randomizing particles lifetime/timers (no fade) (#1)
    * randomizing particles lifetime/timers (no fade)
    
    * create a "variable" with a maximum particles

--- scripts/ButterParticles.gd
+++ scripts/ButterParticles.gd
@@ -1,18 +1,19 @@
 extends Particles2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+const MAXIMUM_NUMBER_OF_PARTICLES = 150
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	self.emitting = true
 	self.one_shot = true
-	self.lifetime = 10
-	get_node("ButterParticlesLifetime").wait_time = 15
+	self.lifetime = rand_range(5, 25)
+	get_node("ButterParticlesLifetime").wait_time = rand_range(10, 35)
 
 
 func _on_ButterParticlesLifetime_timeout():
-	queue_free()
+	var n_particles = get_parent().get_children().size()
+	
+	if n_particles <= MAXIMUM_NUMBER_OF_PARTICLES:
+		get_node("ButterParticlesLifetime").start(rand_range(5, 15))
+	else:
+		queue_free()
