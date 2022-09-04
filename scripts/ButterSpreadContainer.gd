7cc3d4883fdf91f3475765bb9e05037a7269a0c3
piokli <pixelarious@gmail.com>
2022-09-04 13:38:34 +0200

    butter with ketchup

--- scripts/ButterSpreadContainer.gd
+++ scripts/ButterSpreadContainer.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+# "0" is multi node, "1" is single node (newer version) 
 var butter_version = 0
 
 var butter_scene = load("res://scenes/ButterSpreadParticle.tscn")
@@ -7,6 +8,7 @@ var butter_scene_v2 = load("res://scenes/ButterSpreadParticle_v2.tscn")
 var butter_instance_v2 = butter_scene_v2.instance()
 
 func _ready():
+	butter_instance_v2.emitting = false
 	get_node("ViewportContainer/Viewport").add_child(butter_instance_v2)
 
 
