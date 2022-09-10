c268fa41893207bf3c3c9b7d290122cfbe5830c4
piokli <pixelarious@gmail.com>
2022-09-10 16:06:38 +0200

    f1 for help-ugly fix, but enough for this purpose

--- scripts/DebuggingTools.gd
+++ scripts/DebuggingTools.gd
@@ -12,18 +12,22 @@ func _ready():
 func _process(_delta):
 	if Input.is_action_just_pressed("db_f1"):
 		print("db_f1")
-		if TileMapMaskHide == 1:
-			get_node("/root/World/Game/TileMapMask").hide()
-			TileMapMaskHide = 0
-		else:
-			get_node("/root/World/Game/TileMapMask").show()
-			TileMapMaskHide = 1
+		print("f1 - help\nf2 - butter version\nf3 - mask hide/show")
 	elif Input.is_action_just_pressed("db_f2"):
 		print("db_f2")
 		ButterSpreadVersion = 0 if ButterSpreadVersion == 1 else 1
 		get_node("/root/World/Game/ButterSpreadContainer").butter_version = ButterSpreadVersion
+		print("butter version: ", ButterSpreadVersion)
 	elif Input.is_action_just_pressed("db_f3"):
 		print("db_f3")
+		if TileMapMaskHide == 1:
+			get_node("/root/World/Game/TileMapMask").hide()
+			TileMapMaskHide = 0
+			print("mask hide")
+		else:
+			get_node("/root/World/Game/TileMapMask").show()
+			TileMapMaskHide = 1
+			print("mask show")
 	elif Input.is_action_just_pressed("db_f4"):
 		print("db_f4")
 	elif Input.is_action_just_pressed("db_f5"):
