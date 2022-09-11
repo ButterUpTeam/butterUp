451dea779e9b0b015b3daef1cb0cc80dc300881a
piokli <pixelarious@gmail.com>
2022-09-11 15:55:31 +0200

    deleting unused files
    leaving only ButterSpread_v2, F2 does nothing now

--- scripts/GamePlay.cs
+++ scripts/GamePlay.cs
@@ -42,24 +42,10 @@ public class GamePlay : Node
 			player.Idle();
 		}
 
-		var butter_spread = ResourceLoader.Load<PackedScene>("res://scenes/ButterSpread.tscn");
-
 		if ((player.IsMoving() && player.GetSlideCount() > 0) || player.IsOnCeiling() == true) 
 		{
 			//https://www.youtube.com/watch?v=RjVel3Ms9wo
 			this.EmitSignal("Moved", player.GlobalPosition.x, player.GlobalPosition.y);
-
-			// var butterSpread_instance = butter_spread.InstanceOrNull<ButterSpread>();
-			// if (butterSpread_instance != null)
-			// {
-			// 	GetTree().CurrentScene.AddChild(butterSpread_instance);
-			// 	butterSpread_instance.GlobalPosition = new Vector2(player.GlobalPosition.x, player.GlobalPosition.y);
-			// 	butterSpread_instance.Emitting = true;
-			// }
-			// else
-			// {
-			// 	GD.Print("Couldnt instance butterspread!");
-			// }
 		}
 	}
 }
