b0d17f9ad22a2f23d6d18bfa56949956debe45dc
piokli <pixelarious@gmail.com>
2022-09-25 19:42:13 +0200

    one_shot - requested change

--- scripts/ButterSpreadContainer.gd
+++ scripts/ButterSpreadContainer.gd
@@ -20,4 +20,6 @@ func _on_GamePlay_Moved(newx, newy):
 	elif butter_version == 1:
 		butter_instance_v2.position = Vector2(newx, newy)
 		butter_instance_v2.emitting = true
+		butter_instance_v2.get_node("Particles2D_front").emitting = true
+		butter_instance_v2.get_node("Particles2D_base").emitting = true
 	
