329a0ed816ea60b343f7a1c6405c563c4db0c1c3
piokli <pixelarious@gmail.com>
2022-09-26 22:35:38 +0200

    duplication

--- scripts/ButterSpreadContainer.gd
+++ scripts/ButterSpreadContainer.gd
@@ -4,21 +4,19 @@ extends Node2D
 var butter_scene_v2 = preload("res://scenes/ButterSpreadParticle_v2.tscn")
 var butter_instance_v2 = butter_scene_v2.instance()
 
+func spread_some_butter(x, y):
+	butter_instance_v2.position = Vector2(x, y)
+	butter_instance_v2.emitting = true
+	butter_instance_v2.get_node("Particles2D_front").emitting = true
+	butter_instance_v2.get_node("Particles2D_base").emitting = true
 
 func _ready():
 	butter_instance_v2.emitting = false
 	get_node("ViewportContainer/Viewport").add_child(butter_instance_v2)
 	
 func _on_Player_Moved(newx, newy):
-	butter_instance_v2.position = Vector2(newx, newy)
-	butter_instance_v2.emitting = true
-	butter_instance_v2.get_node("Particles2D_front").emitting = true
-	butter_instance_v2.get_node("Particles2D_base").emitting = true
+	spread_some_butter(newx, newy)
 	
 func _on_Player_JustFalled(newx, newy):
 	print("Player Falled")
-	
-	butter_instance_v2.position = Vector2(newx, newy)
-	butter_instance_v2.emitting = true
-	butter_instance_v2.get_node("Particles2D_front").emitting = true
-	butter_instance_v2.get_node("Particles2D_base").emitting = true
+	spread_some_butter(newx, newy)
